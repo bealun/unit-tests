30b554de00a8c3657b1c961f3608762deaf0ecee
bealun <53275597+bealun@users.noreply.github.com>
2020-03-10 13:57:33 +0100

    5&6 done

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -1,8 +1,23 @@
 const months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
+  'January', 
+  'February', 
+  'March', 
+  'April', 
+  'May', 
+  'June',
+  'July', 
+  'August', 
+  'September', 
+  'October', 
+  'November', 
+  'December'
 ]
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
+
+  if (monthNumber > 12 || monthNumber == 0) {
+    return null
+  } else {
+  return months[monthNumber - 1]
+  }
 }
